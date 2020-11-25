f934750bc868db5aafd4b92e46b1a40dad39fe2c
afabiani <alessio.fabiani@geo-solutions.it>
2020-11-25 21:02:50 +0100

    - Battle Schedule: Adding All Events Pane

--- battle_schedule/views.py
+++ battle_schedule/views.py
@@ -25,9 +25,31 @@ def battle_schedule(request, template='battle-schedule.html'):
         territory__takeover_day=tomorrow).order_by('territory__takeover_time')
     battle_events_day_after = BattleEvent.objects.filter(
         territory__takeover_day=day_after).order_by('territory__takeover_time')
+
+    battle_events_sunday = BattleEvent.objects.filter(
+        territory__takeover_day=1).order_by('territory__takeover_time')
+    battle_events_monday = BattleEvent.objects.filter(
+        territory__takeover_day=2).order_by('territory__takeover_time')
+    battle_events_tuesday = BattleEvent.objects.filter(
+        territory__takeover_day=3).order_by('territory__takeover_time')
+    battle_events_wednesday = BattleEvent.objects.filter(
+        territory__takeover_day=4).order_by('territory__takeover_time')
+    battle_events_thursday = BattleEvent.objects.filter(
+        territory__takeover_day=5).order_by('territory__takeover_time')
+    battle_events_friday = BattleEvent.objects.filter(
+        territory__takeover_day=6).order_by('territory__takeover_time')
+    battle_events_saturday = BattleEvent.objects.filter(
+        territory__takeover_day=7).order_by('territory__takeover_time')
     ctx = {
         'battle_events_today': battle_events_today,
         'battle_events_tomorrow': battle_events_tomorrow,
         'battle_events_day_after': battle_events_day_after,
+        'battle_events_sunday': battle_events_sunday,
+        'battle_events_monday': battle_events_monday,
+        'battle_events_tuesday': battle_events_tuesday,
+        'battle_events_wednesday': battle_events_wednesday,
+        'battle_events_thursday': battle_events_thursday,
+        'battle_events_friday': battle_events_friday,
+        'battle_events_saturday': battle_events_saturday,
     }
     return render(request, template, context=ctx)
